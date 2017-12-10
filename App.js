3e5fde927342d695ce5e21dfc491686f1b38f199
Victor Moura <victor_cmoura@hotmail.com>
2017-12-10 02:13:12 -0200

    Delete ToDo + user feedback improvements

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Expo from 'expo';
 import NavigationBar from 'react-native-navbar';
 import {
   StyleSheet,
@@ -8,8 +9,13 @@ import {
   SectionList,
   ListView,
   StatusBar,
-  ScrollView
+  ScrollView,
+  Alert
 } from 'react-native';
+import {
+  Toast,
+  Root
+} from 'native-base';
 import { List, ListItem } from 'react-native-elements';
 import * as firebase from 'firebase';
 import { config } from './secrets'
@@ -20,10 +26,20 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       list: [],
-      newToDo: ""
+      newToDo: "",
+      selectedToDo: {
+        index: null,
+        title: ""
+      }
     };
 
     firebase.initializeApp(config);
+  }
+
+  componentWillMount() {
+    Expo.Font.loadAsync({
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+    });
   }
 
   componentDidMount() {
@@ -53,9 +69,42 @@ export default class App extends React.Component {
       rootRef.set({
         list: newList
       });
-      
-      alert("ToDo created");
+
+      Toast.show({
+        text: 'ToDo created',
+        position: 'bottom',
+        buttonText: 'Ok'
+      });
     }
+  }
+
+  deleteElementFromList(index) {
+    const rootRef = firebase.database().ref("data");
+
+    let newList = this.state.list;
+
+    newList.splice(index, 1);
+
+    rootRef.set({
+      list: newList
+    });
+
+    Toast.show({
+      text: 'ToDo deleted',
+      position: 'bottom',
+      buttonText: 'Ok'
+    });
+  }
+
+  deleteElementAlert(element, index) {
+    Alert.alert(
+      "Delete ToDo",
+      "Do you want to delete " + '"' + element + '"' + "?",
+      [
+        { text: 'Delete', onPress: () => this.deleteElementFromList(index) },
+        { text: 'Cancel'}
+      ]
+    )
   }
 
   handleFieldOnChange(field, value) {
@@ -66,43 +115,46 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <StatusBar hidden={true} />
-        <NavigationBar
-          title={{ title: "Personal ToDo" }}
-          style={styles.navBar}
-        />
-        <ScrollView
-          style={styles.scrollView}
-          showsHorizontalScrollIndicator={true}
-          alwaysBounceHorizontal={true}
-        >
-          <List style={styles.list}>
-            {
-              this.state.list.map((element, i) => {
-                return (
-                  <ListItem
-                    key={i}
-                    title={element}
-                    style={styles.listItem}
-                    hideChevron={true}
-                  />
-                );
-              })
-            }
-            <ListItem
-              style={styles.listItem}
-              hideChevron={false}
-              subtitle={"Add ToDo"}
-              textInput={true}
-              textInputPlaceholder={"Type here your new ToDo"}
-              textInputReturnKeyType={"send"}
-              textInputOnChangeText={(text) => this.handleFieldOnChange("newToDo", text)}
-              onPress={() => this.writeNewElement(this.state.newToDo)}
-            />
-          </List>
-        </ScrollView>
-      </View>
+      <Root>
+        <View style={styles.container}>
+          <StatusBar hidden={true} />
+          <NavigationBar
+            title={{ title: "Personal ToDo" }}
+            style={styles.navBar}
+          />
+          <ScrollView
+            style={styles.scrollView}
+            showsHorizontalScrollIndicator={true}
+            alwaysBounceHorizontal={true}
+          >
+            <List style={styles.list}>
+              {
+                this.state.list.map((element, i) => {
+                  return (
+                    <ListItem
+                      key={i}
+                      title={element}
+                      style={styles.listItem}
+                      hideChevron={true}
+                      onLongPress={() => this.deleteElementAlert(element, i)}
+                    />
+                  );
+                })
+              }
+              <ListItem
+                style={styles.listItem}
+                hideChevron={false}
+                subtitle={"Add ToDo"}
+                textInput={true}
+                textInputPlaceholder={"Type here your new ToDo"}
+                textInputReturnKeyType={"send"}
+                textInputOnChangeText={(text) => this.handleFieldOnChange("newToDo", text)}
+                onPress={() => this.writeNewElement(this.state.newToDo)}
+              />
+            </List>
+          </ScrollView>
+        </View>
+      </Root>
     );
   }
 }
